b166b3073d369ed091f824adacd4ee7b66ef7aca
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2021-10-09 22:38:08 +0530

    fix: refactored tests

--- main_test.go
+++ main_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-
+	"net/http"
 	"net/http/httptest"
 	"testing"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -12,28 +12,14 @@ import (
 	"github.com/devashar13/instagram-api/utils"
 	"context"
 	"log"
-	"net/http"
+	"bytes"
 	"fmt"
 	"time"
-	"bytes"
 
 )
 
-func CreateUser(t *testing.T) {
-	var jsonStr = []byte(`{"Name":"TestUser","Email":"test@admin.com","password":"admin"}`)
-	req, err := http.NewRequest("POST", "/entry", bytes.NewBuffer(jsonStr))
-	if err != nil {
-		t.Fatal(err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-}
-
-
-
-
 func GetUser(t *testing.T) {
-	// uh := newUserHandler()
+	uh := newUserHandler()
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
 	id := primitive.NewObjectID()
 	user := models.User{ID:id,Name:"Dev",Email:"devashar13@gmail.com",Password:"password"}
@@ -52,41 +38,28 @@ func GetUser(t *testing.T) {
 	userCollection := quickstartDatabase.Collection("users")
 	resultInsertionNumber, insertErr := userCollection.InsertOne(ctx, user)
 	fmt.Println(resultInsertionNumber,insertErr)
-
-}
-
-
-func checkStatusCode(code int, want int, t *testing.T) {
-	if code != want {
-		t.Errorf("Wrong status code: got %v want %v", code, want)
+	req, _ := http.NewRequest("GET", "/users"+id.Hex(), nil)
+	r := httptest.NewRecorder()
+	uh.ServeHTTP(r,req)
+	if status := r.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
 	}
-}
-
-func checkContentType(r *httptest.ResponseRecorder, t *testing.T) {
-	ct := r.Header().Get("Content-Type")
-	if ct != "application/json" {
-		t.Errorf("Wrong Content Type: got %v want application/json", ct)
+	expected := `{"id":4,"first_name":"xyz","last_name":"pqr","email_address":"xyz@pqr.com","phone_number":"1234567890"}`
+	if r.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			r.Body.String(), expected)
 	}
+
 }
 
+func CreateUser(t *testing.T) {
+	var jsonStr = []byte(`{"Name":"TestUser","Email":"test@admin.com","password":"admin"}`)
+	req, err := http.NewRequest("POST", "/entry", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
 
-// func checkProperties(st student, t *testing.T) {
-// 	if st.Name != "John Doe" {
-// 		t.Errorf("Name should match: got %v want %v", st.Name, "Peter Doe")
-// 	}
-// 	if st.Age != 20 {
-// 		t.Errorf("Age should match: got %v want %v", st.Age, 20)
-// 	}
-// }
-
-// func checkBody(body *bytes.Buffer, st student, t *testing.T) {
-// 	var students []student
-// 	_ = json.Unmarshal(body.Bytes(), &students)
-// 	if len(students) != 1 {
-// 		t.Errorf("Wrong lenght: got %v want 1", len(students))
-// 	}
-// 	if students[0] != st {
-// 		t.Errorf("Wrong body: got %v want %v", students[0], st)
-// 	}
-// }
+}
 
