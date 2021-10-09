8d090033492cfc74a6a37c63b9ad1ecc15c3e71b
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2021-10-09 17:57:02 +0530

    feat:user and post endpoints

--- main.go
+++ main.go
@@ -5,15 +5,18 @@ package main
 import (
 	"log"
 	"context"
+	"io"
+
 	"time"
     "net/http"
 	"encoding/json"
-	"errors"
+
 	"fmt"
-	"strconv"
+
 	"strings"
 	"sync"
-
+	"go.mongodb.org/mongo-driver/bson"
+	"crypto/sha256"
 	"io/ioutil"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -25,6 +28,9 @@ import (
 type userHandler struct {
 	sync.Mutex
 }
+type postHandler struct {
+	sync.Mutex
+}
 
 type User struct {
     ID            primitive.ObjectID `bson:"_id,omitempty"`
@@ -33,27 +39,185 @@ type User struct {
     Password string `json:"password" bson:"password"`
 }
 
+type Post struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	User     primitive.ObjectID `bson:"user"`
+	Caption       string             `json:"caption" bson:"caption"`
+	Imageurl string            `json:"imageurl" bson:"imageurl"`
+	CreatedDate time.Time   `json:"time" bson:"time"`
+}
 
-func (ph *userHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+
+
+func (uh *userHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		ph.get(w, r)
+		uh.get(w, r)
 	case "POST":
-		ph.post(w, r)
+		uh.post(w, r)
 	case "PUT", "PATCH":
-		ph.put(w, r)
+		uh.put(w, r)
 	case "DELETE":
-		ph.delete(w, r)
+		uh.delete(w, r)
 	default:
 		respondWithError(w, http.StatusMethodNotAllowed, "invalid method")
 	}
 }
 
 
-func (ph *userHandler) get(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Get Func")
+func (ph *postHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		ph.get(w, r)
+	case "POST":
+		ph.post(w, r)
+	case "PUT", "PATCH":
+		// ph.put(w, r)
+	case "DELETE":
+		// ph.delete(w, r)
+	default:
+		respondWithError(w, http.StatusMethodNotAllowed, "invalid method")
+	}
+}
+
+// Post Controlls
+func (ph *postHandler) post(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type","application/json")   
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	ct := r.Header.Get("content-type")
+	if ct != "application/json" {
+		respondWithError(w, http.StatusUnsupportedMediaType, "content type 'application/json' required")
+		return
+	}
+	var post Post
+	post.ID = primitive.NewObjectID()
+	err = json.Unmarshal(body, &post)
+    // getHash(user.Password)
+	createPost(post)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
 
 }
+
+func (ph *postHandler) get(w http.ResponseWriter, r *http.Request) {
+	defer ph.Unlock()
+	ph.Lock()
+	id, err := idFromUrl(r)
+	if err != nil {
+	}
+	if strings.Contains(r.URL.String(),"users"){
+			objectID, _ := primitive.ObjectIDFromHex(id)
+			client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+			ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+			err = client.Connect(ctx)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer client.Disconnect(ctx)
+			quickstartDatabase := client.Database("instagramapi")
+			postCollection := quickstartDatabase.Collection("posts")
+			cursor, err := postCollection.Find(ctx, bson.M{"user": objectID})
+			if err != nil {
+				log.Fatal(err)
+			}
+			var posts []bson.M
+			if err = cursor.All(ctx, &posts); err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(posts)
+		// hi
+	}else{
+		
+		objectID, _ := primitive.ObjectIDFromHex(id)
+		client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		err = client.Connect(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer client.Disconnect(ctx)
+		quickstartDatabase := client.Database("instagramapi")
+		postCollection := quickstartDatabase.Collection("posts")
+		var post bson.M
+		if err = postCollection.FindOne(ctx, bson.M{"_id": objectID}).Decode(&post); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(post)
+
+	}
+	
+	
+
+}
+
+
+
+func createPost(post Post){
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	quickstartDatabase := client.Database("instagramapi")
+	postCollection := quickstartDatabase.Collection("posts")
+	userCollection := quickstartDatabase.Collection("users")
+	var user bson.M
+	if err = userCollection.FindOne(ctx, bson.M{"_id": post.User}).Decode(&user); err != nil {
+		log.Fatal(err)
+	}
+	if user != nil{
+		post.CreatedDate = time.Now()
+		resultInsertionNumber, insertErr := postCollection.InsertOne(ctx, post)
+		fmt.Println(resultInsertionNumber,insertErr)
+	}
+}
+
+
+
+
+// User Controls 
+
+func (ph *userHandler) get(w http.ResponseWriter, r *http.Request) {
+	defer ph.Unlock()
+	ph.Lock()
+	id, err := idFromUrl(r)
+	if err != nil {
+	}
+	objectID, _ := primitive.ObjectIDFromHex(id)
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+    err = client.Connect(ctx)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer client.Disconnect(ctx)
+	quickstartDatabase := client.Database("instagramapi")
+	userCollection := quickstartDatabase.Collection("users")
+	var user bson.M
+	if err = userCollection.FindOne(ctx, bson.M{"_id": objectID}).Decode(&user); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+
 
 func (ph *userHandler) post(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type","application/json")   
@@ -70,6 +234,7 @@ func (ph *userHandler) post(w http.ResponseWriter, r *http.Request) {
 	var user User
 	user.ID = primitive.NewObjectID()
 	err = json.Unmarshal(body, &user)
+    // getHash(user.Password)
 	createUser(user)
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, err.Error())
@@ -78,7 +243,6 @@ func (ph *userHandler) post(w http.ResponseWriter, r *http.Request) {
 
 
     // var user User  = json.NewDecoder(r.Body).Decode(&user)  
-    // user.Password = getHash([]byte(user.Password)) 
     // collection := client.Database("GODB").Collection("user") 
     // ctx,_ := context.WithTimeout(context.Background(),      
     //          10*time.Second) 
@@ -94,13 +258,11 @@ func (ph *userHandler) post(w http.ResponseWriter, r *http.Request) {
 }
 
 
-// func getHash(pwd []byte) string {        
-//     hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)          
-//     if err != nil {
-//        log.Println(err)
-//     }
-//     return string(hash)
-// }
+func getHash(pwd string){        
+    h := sha256.New()
+	io.WriteString(h, pwd)
+
+}
 
 func (ph *userHandler) put(w http.ResponseWriter, r *http.Request) {
 	
@@ -126,25 +288,29 @@ func respondWithJSON(w http.ResponseWriter, code int, data interface{}) {
 
 
 
-func idFromUrl(r *http.Request) (int, error) {
+func idFromUrl(r *http.Request) (string, error) {
 	parts := strings.Split(r.URL.String(), "/")
-	if len(parts) != 3 {
-		return 0, errors.New("not found")
+	fmt.Println("hello",parts)
+	if len(parts) > 3{
+		id := parts[3]
+		return id, nil
 	}
-	id, err := strconv.Atoi(parts[len(parts)-1])
-	if err != nil {
-		return 0, errors.New("not found")
-	}
+	id := parts[2]
 	return id, nil
 }
-
-func newProductHandler() *userHandler {
+func newUserHandler() *userHandler {
 	return &userHandler{}
 
 }
 
+func newPostHandler() *postHandler {
+	return &postHandler{}
+
+}
+
+
+
 func createUser(user User){
-	fmt.Println(user)
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
 		log.Fatal(err)
@@ -163,19 +329,28 @@ func createUser(user User){
 	userCollection := quickstartDatabase.Collection("users")
 	resultInsertionNumber, insertErr := userCollection.InsertOne(ctx, user)
 	fmt.Println(resultInsertionNumber,insertErr)
+
 }
 
 
 func main(){
 	
 
-	ph := newProductHandler()
+	uh := newUserHandler()
+	ph := newPostHandler()
+
 	port := ":5000"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
-	http.Handle("/products", ph)
-	http.Handle("/user", ph)
+
+	http.Handle("/users", uh)
+	http.Handle("/users/", uh)
+	http.Handle("/posts", ph)
+	http.Handle("/posts/users/", ph)
+	http.Handle("/posts/", ph)
+
+
 	fmt.Println("Starting server on port", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 
--- models/models.go
+++ models/models.go
@@ -9,10 +9,7 @@ import (
 //User is the model that governs all notes objects retrived or inserted into the DB
 type User struct {
     ID            primitive.ObjectID `bson:"_id"`
-    First_name    *string            `json:"first_name" validate:"required,min=2,max=100"`
-    Last_name     *string            `json:"last_name" validate:"required,min=2,max=100"`
-    Password      *string            `json:"Password" validate:"required,min=6""`
-    Email         *string            `json:"email" validate:"email,required"`
-    Phone         *string            `json:"phone" validate:"required"`
-    User_id       string             `json:"user_id"`
+    Name string `json:"name" bson:"name"` 
+    Email string `json:"email" bson:"email"` 
+    Password string `json:"password" bson:"password"`
 }
